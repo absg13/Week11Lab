6f3cb6f46bbe710c73e5bea0d16f8014c8b43eb6
738377 <738377@ELF6215897.ACDM.DS.SAIT.CA>
2017-11-03 14:41:44 -0600

    Use this one. Midterm

--- SAIT Class Analyzer/src/java/servlets/HomeServlet.java
+++ SAIT Class Analyzer/src/java/servlets/HomeServlet.java
@@ -5,12 +5,15 @@
  */
 package servlets;
 
+import business.Class;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -18,33 +21,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class HomeServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HomeServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -56,7 +32,8 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").
+                forward(request, response);
     }
 
     /**
@@ -70,17 +47,85 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        HttpSession session = request.getSession();
+        String name = request.getParameter("name");
+        String code = request.getParameter("code");
+        String currentgrade = request.getParameter("currentgrade");
+        String desiredgrade = request.getParameter("desiredgrade");
+        ArrayList<Class> classes = new ArrayList<>();
+
+        if (request.getParameter("pageName").equals("add")) {
+
+            if (name == null || code == null || currentgrade == null || desiredgrade == null) {
+                request.setAttribute("classes", classes);
+                session.setAttribute("classes", classes);
+                getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").
+                        forward(request, response);
+
+                return;
+            } else if (name.isEmpty() || code.isEmpty() || currentgrade.isEmpty() || desiredgrade.isEmpty()) {
+                request.setAttribute("classes", classes);
+                session.setAttribute("classes", classes);
+                request.setAttribute("message", "Invalid entry.");
+
+                getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").
+                        forward(request, response);
+
+                return;
+            } else if (!currentgrade.matches("^-?\\d+$")
+                    || !desiredgrade.matches("^-?\\d+$")) {
+                request.setAttribute("message", "Invalid entry.");
+
+                getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").
+                        forward(request, response);
+
+                return;
+            }
+
+            int currentGrade = Integer.parseInt(currentgrade);
+            int desiredGrade = Integer.parseInt(desiredgrade);
+
+            if (currentGrade < 0 || currentGrade > 100) {
+                request.setAttribute("message", "Invalid entry.");
+
+                getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").
+                        forward(request, response);
+
+                return;
+            } else if (desiredGrade < 0 || desiredGrade > 100) {
+                request.setAttribute("message", "Invalid entry.");
+
+                getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").
+                        forward(request, response);
+
+                return;
+            }
+
+            classes.add(new Class(name, code, currentGrade, desiredGrade));
+            request.setAttribute("classes", classes);
+            session.setAttribute("classes", classes);
+            request.setAttribute("message", "Class added");
+
+        } else if (request.getParameter("pageName").equals("delete")) {
+
+            for (Class c : classes) {
+
+                String deleted = request.getParameter("delete_" + c.getName());
+
+                if (c.getName().equals(deleted)) {
+                    classes.remove(c);
+
+                    session.setAttribute("classes", classes);
+                    return;
+                }
+            }
+
+            request.setAttribute("message", "Class deleted");
+        }
+
+        request.setAttribute("classes", classes);
+        session.setAttribute("classes", classes);
+        getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").
+                forward(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
--- SAIT Class Analyzer/web/WEB-INF/home.jsp
+++ SAIT Class Analyzer/web/WEB-INF/home.jsp
@@ -5,13 +5,69 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>SAIT Class Analyzer</title>
     </head>
+    
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 5px;
+            text-align: left;    
+        }
+        <%-- css styling from: 
+            https://www.w3schools.com/html/tryit.asp?filename=tryhtml_table_colspan
+        --%>
+    </style>
+    
     <body>
-        <h1>Hello World!</h1>
+        <h1>SAIT Class Analyzer</h1>
+        <h2>Add Class</h2>
+        
+        <div>
+                <form action="home" method="POST">
+                    <input type="hidden" name="pageName" id="pageName" value="add"/>
+                    Class name: <input type="text" name="name" value="${name}"><br>
+                    Class code: <input type="text" name="code" value="${code}"><br>
+                    Current grade: <input type="text" name="currentgrade" value="${currentgrade}"><br>
+                    Desired grade: <input type="text" name="desiredgrade" value="${desiredgrade}"><br>
+                    <input type="submit" value="Add"><br><br>
+            </form>
+            </div> 
+                    ${message} <br><br>
+                
+             
+             <form action="home" method="POST">
+             <input type="hidden" name="pageName" id="pageName" value="delete"/>
+
+            <table style="width:25%">
+               <tr>
+                 <th>Name</th>
+                 <th>Code</th> 
+                 <th>Current Grade</th>
+                 <th>Desired Grade</th>
+               </tr>
+               
+               <c:forEach var="item" items="${classes}">
+               <tr>
+                 <td>${item.name}</td>
+                 <td>${item.code}</td>
+                 <td>${item.currentgrade}</td>
+                 <td>${item.desiredgrade}</td>
+                 <td><input type="radio" name="delete_${item.name}" value="${item.name}"></td>
+               </tr>
+               </c:forEach>
+               
+               <tr> <td><input type="submit" name="deletePost" value="Delete"></td></tr>
+                   
+             </table>
+          </form>
     </body>
 </html>
